1cfa3c36d13a6463ffe21d57e9b7c4b8157b3ede
Christian <ccabrer@clemson.edu>
2021-05-20 16:28:48 -0400

    Done making PongGame into a view class.

--- app/src/main/java/com/christian/pong/PongGame.java
+++ app/src/main/java/com/christian/pong/PongGame.java
@@ -1,7 +1,12 @@
 package com.christian.pong;
 
+import android.content.Context;
 import android.view.SurfaceView;
 
 public class PongGame extends SurfaceView {
 
+    //constructor of PongGame
+    public PongGame(Context context, int x, int y){
+        super(context);
+    }
 }
