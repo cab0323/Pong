20ea8b1316037010a0c01f5ef6a920b153526857
Christian <ccabrer@clemson.edu>
2021-05-20 16:38:11 -0400

    Done adding the variables in PongGame class.

--- app/src/main/java/com/christian/pong/PongGame.java
+++ app/src/main/java/com/christian/pong/PongGame.java
@@ -1,9 +1,32 @@
 package com.christian.pong;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class PongGame extends SurfaceView {
+    //variables ////////////////
+    private final boolean DEBUGGING = true;
+    //drawing variables
+    private SurfaceHolder mOurHolder;
+    private Canvas mCanvas;
+    private Paint mPaint;
+    //frames per second
+    private long mFPS;
+    //milliseconds in a second
+    private final int MILLIS_IN_SECOND = 1000;
+    //screen resolution
+    private int mScreenX;
+    private int mScreenY;
+    //text size
+    private int mFontSize;
+    private int mFontMargin;
+    //game objects
+    private Bat mBat;
+    private Ball mBall;
+    //score and lives remaining
+    private int mScore;
+    private int mLives;
 
     //constructor of PongGame
     public PongGame(Context context, int x, int y){
