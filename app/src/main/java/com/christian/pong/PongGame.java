4700b32acca7c9638d80bed46b002818b4726427
Christian <ccabrer@clemson.edu>
2021-05-20 16:52:21 -0400

    Finished constructor of PongGame class. Starting the startNewGame method.

--- app/src/main/java/com/christian/pong/PongGame.java
+++ app/src/main/java/com/christian/pong/PongGame.java
@@ -1,8 +1,10 @@
 package com.christian.pong;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.graphics.Canvas;
 
 public class PongGame extends SurfaceView {
     //variables ////////////////
@@ -31,5 +33,30 @@ public class PongGame extends SurfaceView {
     //constructor of PongGame
     public PongGame(Context context, int x, int y){
         super(context);
+
+        //initialize screen resolution
+        //this also gives our entire pong class access to the screen resolution
+        mScreenX = x;
+        mScreenY = y;
+
+        //set font to (1/20th) of screen width
+        mFontSize = mScreenX/20;
+        //set margin to (1/75th) of screen width
+        mFontMargin = mScreenX/75;
+        //initializing our objects
+        mOurHolder = getHolder();
+        mPaint = new Paint();
+
+        //start the game
+        startNewGame();
+    }
+
+    //starting a new game
+    private void startNewGame(){
+        //resetting ball to starting position
+
+        //reset player's score and lives
+        mScore = 0;
+        mLives = 3;
     }
 }
